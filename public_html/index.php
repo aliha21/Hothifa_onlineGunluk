a49dca90c06267dd0fa5cadb0f222e0ba8a9ad6c
aliha21 <benimhuzeyfe@gmail.com>
2025-05-28 22:59:51 +0300

    Index: oturumlu kullanıcıları entries.php’ye yönlendir

--- public_html/index.php
+++ public_html/index.php
@@ -1,22 +1,12 @@
 <?php
-// public_html/index.php
 session_start();
+
+// 1. Eğer oturum yoksa login sayfasına gönder
 if (!isset($_SESSION['user_id'])) {
     header('Location: /login.php');
     exit;
 }
-?>
 
-<!DOCTYPE html>
-<html lang="tr">
-<head>
-  <meta charset="UTF-8">
-  <title>Günlüklerim | Online Günlük</title>
-  <link rel="stylesheet" href="css/style.css">
-</head>
-<body>
-  <h1>Hoş geldin, <?= htmlspecialchars($_SESSION['username']) ?></h1>
-  <p><a href="logout.php">Çıkış Yap</a></p>
-  <!-- Buraya CRUD formları ve günlük listesi gelecek -->
-</body>
-</html>
+// 2. Oturum varsa doğrudan entries.php’ye yönlendir
+header('Location: /entries.php');
+exit;
